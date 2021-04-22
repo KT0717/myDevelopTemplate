da1a8fe7b6870f1a890210fae30f1701237af84a
KT0717 <tx2bxn@icloud.com>
2021-04-22 20:52:37 +0900

    init

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,12 +1,22 @@
-// ES6
-'use strict';
+window.addEventListener('load', function() {
+  getTrainList();
+});
 
-const MESSAGE = "Hello World";
-const dist = document.getElementById('dist');
+function getTrainList() {
+  var url = 'https://covid19-japan-web-api.vercel.app/api/v1/statistics';
+  fetch(url)
+  .then(function (data) {
+    return data.json(); // 読み込むデータをJSONに設定
+  })
+  .then(function (json) {
+    for (var i = 0; i < json.length; i++) {
+      var name_ja = json[i].name_ja;
+      var total_count = json[i].total_count;
 
-let greet = () => {
-    console.log(MESSAGE);
-    dist.innerHTML = MESSAGE;
-  };
-
-greet();
+      var row = document.getElementById('train-list').insertRow();
+      row.insertCell().textContent = i + 1;
+      row.insertCell().textContent = name_ja;
+      row.insertCell().textContent = total_count;
+    }
+  });
+}
