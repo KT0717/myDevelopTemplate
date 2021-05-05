e0cd73138f5c21f23840667453e19e2e7ce76120
KT0717 <tx2bxn@icloud.com>
2021-05-05 22:55:27 +0900

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 // プラグインの読み込み
-var gulp = require("gulp");
-var gulpSass = require("gulp-sass");
-var gulpPostcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
+var gulp = require('gulp');
+var gulpSass = require('gulp-sass');
+var gulpPostcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
 var gulpPlumber = require('gulp-plumber');
 var gulpNotify = require('gulp-notify');
 const sourcemaps = require('gulp-sourcemaps');
-const concat = require("gulp-concat");
-const uglify = require("gulp-uglify");
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 
@@ -17,88 +17,90 @@ var source = 'src/';
 
 // Bootstrap sass ディレクトリ
 var bootstrapSass = {
-    in: './node_modules/bootstrap/scss'
+  in: './node_modules/bootstrap/scss'
 };
 
 //JavaScript ディレクトリ
 var js = {
-    'distDir': source + 'dist/',
-    'watchJs': source + 'js/*.js',
-    'ignore': !source + 'js/lib/*.js'
-}
+  'distDir': source + 'dist/',
+  'watchJs': source + 'js/*.js',
+  'ignore': !source + 'js/lib/*.js'
+};
 
 // javascript
 gulp.task('jsTask', function () {
-    return gulp
-        .src([
-            js.watchJs,
-            js.ignore,
-        ])
-        .pipe(concat('main.js'))
-        .pipe(babel({
-            presets: ["@babel/preset-env"]
-        }))
-        .pipe(uglify())
-        .pipe(rename('main.min.js'))
-        .pipe(gulp.dest(js.distDir));
+  return gulp
+    .src([
+      js.watchJs,
+      js.ignore,
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(js.distDir));
 });
 
 // sass、css関連の変数を設定
 var sass = {
-    in: 'scss/**/*scss',
-    out: source + 'css/',
-    watch: 'scss/**/*',
-    sassOpts: {
-        // 圧縮方法（expanded、nested（ネストがインデントされる）、compact（規則集合毎が1行になる）、compressed（全CSSコードが1行になる））
-        outputStyle: 'compressed',
-        // @import機能で利用できるパスを指定
-        includePaths: [bootstrapSass.in]
-    }
+  in: 'scss/**/*scss',
+  out: source + 'css/',
+  watch: 'scss/**/*',
+  sassOpts: {
+    // 圧縮方法（expanded、nested（ネストがインデントされる）、compact（規則集合毎が1行になる）、compressed（全CSSコードが1行になる））
+    outputStyle: 'compressed',
+    // @import機能で利用できるパスを指定
+    includePaths: [bootstrapSass.in]
+  }
 };
 
 // sassをコンパイルするタスク
 gulp.task('sass', function () {
-    // コンパイル対象のsassディレクトリを指定
-    return gulp
-        .src(sass.in)
-        // ソースマップ作成
-        .pipe(sourcemaps.init())
-        // コンパイルエラー時、エラーメッセージをデスクトップ通知
-        .pipe(gulpPlumber({
-            errorHandler: gulpNotify.onError("Error: <%= error.message %>")
-        }))
-        // コンパイル実行（sass->css）
-        .pipe(gulpSass(sass.sassOpts))
-        // ベンダープレフェックス付与
-        .pipe(gulpPostcss([autoprefixer()]))
-        // ソースマップ書き込み
-        .pipe(sourcemaps.write('./'))
-        // cssをcssディレクトリに出力
-        .pipe(gulp.dest(sass.out))
-        .pipe(browserSync.stream())
-        // コンパイル成功時、正常メッセージをデスクトップ通知
-        .pipe(gulpNotify({
-            message: 'Finished sass',
-            sound: false,
-        }));
+  // コンパイル対象のsassディレクトリを指定
+  return gulp
+    .src(sass.in)
+    // ソースマップ作成
+    .pipe(sourcemaps.init())
+    // コンパイルエラー時、エラーメッセージをデスクトップ通知
+    .pipe(gulpPlumber({
+      errorHandler: gulpNotify.onError('Error: <%= error.message %>')
+    }))
+    // コンパイル実行（sass->css）
+    .pipe(gulpSass(sass.sassOpts))
+    // ベンダープレフェックス付与
+    .pipe(gulpPostcss([autoprefixer()]))
+    // ソースマップ書き込み
+    .pipe(sourcemaps.write('./'))
+    // cssをcssディレクトリに出力
+    .pipe(gulp.dest(sass.out))
+    .pipe(browserSync.stream())
+    // コンパイル成功時、正常メッセージをデスクトップ通知
+    .pipe(gulpNotify({
+      message: 'Finished sass',
+      sound: false,
+    }));
 });
 
 // browser-syncの初期設定
 gulp.task('browser-sync', function () {
-    browserSync.init({
-        server: {
-            // browser-syncのコンテキストルートを指定
-            baseDir: 'src/'
-        },
-        // 最初に開くページを指定
-        startPath: 'index.html'
-    });
+  browserSync.init({
+    server: {
+      // browser-syncのコンテキストルートを指定
+      baseDir: 'src/'
+    },
+    // 最初に開くページを指定
+    startPath: 'index.html'
+  });
 
-    // sass,JS,ディレクトリを監視し、更新があれば自動コンパイルしてブラウザに反映
-    gulp.watch(sass.watch, gulp.series('sass'));
-    gulp.watch(js.watchJs, gulp.series('jsTask'));
-    // htmlディレクトリ、jsディレクトリを監視し、更新があればブラウザをリロード
-    gulp.watch(['src/' + '*html', source + 'css/*', source + 'js/*']).on('change', browserSync.reload);
+  // sass,JS,ディレクトリを監視し、更新があれば自動コンパイルしてブラウザに反映
+  gulp.watch(sass.watch, gulp.series('sass'));
+  gulp.watch(js.watchJs, gulp.series('jsTask'));
+  // htmlディレクトリ、jsディレクトリを監視し、更新があればブラウザをリロード
+  gulp.watch(['src/' + '*html', source + 'css/*', source + 'js/*']).on('change', browserSync.reload);
 
 });
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,101 @@
-// ES6
-'use strict';
+var form = document.getElementById('form');
+var yourName = document.getElementById('your-name');
+var yourEmail = document.getElementById('your-email');
+var yourGender = document.getElementById('your-gender');
+var yourTel = document.getElementById('your-tel');
+var mSex = document.getElementById('msex');
+var fSex = document.getElementById('fsex');
 
-const MESSAGE = "Hello World";
-const dist = document.getElementById('dist');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (validateName(yourName)) {
+    //
+  }
+  if (validateEmail(yourEmail)) {
+    //
+  }
+  if (validSex(mSex, fSex)) {
+    //
+  }
+  if (validTel(yourTel.value)) {
+    //
+  }
+  if (emptyCheck(yourName, yourEmail, yourTel)) {
+    //
+  }
+  return false;
+});
 
-let greet = () => {
-    console.log(MESSAGE);
-    dist.innerHTML = MESSAGE;
-  };
+function emptyCheck(...args) {
+  for (let i = 0; i < args.length; i = (i + 1) | 0) {
+    if (args[i].value === '') {
+      args[i].nextElementSibling.innerHTML = '必須項目です';
+    }
+  }
+  return true;
+}
 
-greet();
+function validateName(yourName) {
+  if (yourName.value.length >= 4) {
+    errMsg(yourName, true);
+    return true;
+  } else {
+    errMsg(yourName, false);
+    return false;
+  }
+}
+
+function validateEmail(yourEmail) {
+  var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (yourEmail.value.match(mailformat)) {
+    errMsg(yourEmail, true);
+    return true;
+  } else {
+    errMsg(yourEmail, false);
+    return false;
+  }
+}
+
+function validTel(str) {
+  var phone_array = str.match(/\+?[0-9]+[-\x20]?[0-9]+[-\x20]?[0-9]+[-\x20]?[0-9]+/g);
+  for (var i = 0; phone_array != null && i < phone_array.length; i++) {
+    var tmp = phone_array[i];
+    str = tmp.replace(/[-\x20]/g, '');
+    if (str.length < 10) {
+      continue;
+    }
+  }
+  console.log(str);
+  return str;
+}
+
+function validSex(mSex, fSex) {
+  var x = 0;
+  if (mSex.checked) {
+    x++;
+  }
+  if (fSex.checked) {
+    x++;
+  }
+  if (x == 0) {
+    errMsg(yourGender, false);
+    return false;
+  } else {
+    errMsg(yourGender, true);
+    return true;
+  }
+}
+
+function errMsg(params, flag) {
+  if (flag == true) {
+    params.nextElementSibling.innerHTML = '<span class="text-success">OK<i class="fas fa-check ml-1"></i></span>';
+  } else if (flag == false && params == yourName) {
+    params.nextElementSibling.innerHTML = '4 文字以上で設定してください';
+  } else if (flag == false && params == yourEmail) {
+    params.nextElementSibling.innerHTML = 'メールの形式が正しくありません';
+  } else if (flag == false && params == yourGender) {
+    params.nextElementSibling.innerHTML = 'どちらかを選択してください';
+  } else if (flag == false && params == yourTel) {
+    params.nextElementSibling.innerHTML = '電話番号の形式が正しくありません';
+  }
+}
