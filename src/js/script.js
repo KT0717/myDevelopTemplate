695d499a1942131437d70a1adbebb1a555c63f44
KT0717 <tx2bxn@icloud.com>
2021-07-04 21:08:33 +0900

    step2

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,39 @@
-// ES6
+// 目標物
+{/* <ul id="board">
+    <li class="pressed"></li>
+    <li class="pressed"></li>
+    <li class="pressed"></li>
+    <li class="pressed"></li>
+</ul> */}
+
 'use strict';
 
-const MESSAGE = "a World";
-const dist = document.getElementById('dist');
+{
 
-let greet = () => {
-    console.log(MESSAGE);
-    dist.innerHTML = MESSAGE;
-  };
+  // ２）
+  // Panel クラス
+  // コンストラクターを書く
+  // 要素（li）をプロパティとして持たせたいので、プロパティ名は、 element の el と
 
-greet();
+
+  // １）
+  // Boradクラスを作成
+  // コンストラクターを書く
+  // コンストラクターの処理ですが、パネルを管理したいので、とりあえずプロパティ（this.panels）で配列として持っておきましょう。空の配列を用意
+  // パネルを 4 枚作りたいので、ループ、// this.panels に対してあとで作っていく Panel クラスのインスタンスを push()
+  // 4 つのパネルをページに追加していく、コンストラクターのほうではそれをsetupメソッドとして呼び出すだけ
+
+
+
+  // ３）
+  // setup() メソッドですが、ますは board 要素を取得しておきます。setup() の中でしか使わないので、プロパティにする必要はなくて const で定数で宣言しておきましょう。
+  // そのうえで panels の数だけ要素を追加していけばいいので、 forEach() を使っていきます。ひとつひとつの要素を panel としつつ、 board に対して appendChild() してあげれば OK でしょう。
+  // 今回追加するのは li 要素なので、 panel の el プロパティを追加してあげればよいのですけれども、実はクラスのプロパティに外部から直接アクセスしないほうがよいとされているので、こちらのプロパティはメソッド経由で取得するようにしてあげたほうがいいでしょう。
+  // では、いったんこちらコメントにしてあげて、 getElement という意味で getEl() というメソッドを Panel クラスのほうにあとで作っていきましょう。
+
+
+
+
+
+  // インスタンスを作る
+}
